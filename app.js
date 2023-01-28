31a29bc261b367c7ecc2655d84eaa8faa4fc65fe
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-01-28 18:00:48 -0500

    permissions

--- app.js
+++ app.js
@@ -38,4 +38,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 module.exports = app;
